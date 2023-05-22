93dbbabdf7ad3b9ba8ed71a3c1ace94a3e16cfca
samer tashteba <samer.tashteba@gmail.com>
2023-05-22 14:37:16 +0200

    modification carrosuel

--- slider.js
+++ slider.js
@@ -4,10 +4,10 @@ let numero = 0;
 
 function ChangeSlide(sens) {
     numero = numero + sens;
-    if (numero > slide.length -1)
-    numero = 0;
-    if (numero < slide.lengthc -1)
-    numero = 0;
+    if (numero < 0)
+        numero = slide.length - 1;
+    if (numero > slide.length - 1)
+        numero = 0;
    document.getElementById('slide').src = 'images/Caroussel/' + slide [numero];
 }
 
